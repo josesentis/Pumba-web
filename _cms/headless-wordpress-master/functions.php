a0ce2eed43ba0ee7cc872ea9bd4af9d14fa34166
Jose Sentis <jose.sentis1987@gmail.com>
2022-06-29 20:00:39 +0200

    Widget clients

--- _cms/headless-wordpress-master/functions.php
+++ _cms/headless-wordpress-master/functions.php
@@ -29,7 +29,6 @@ add_image_size( 'twitter', 600, 330, true);
 
 ///// OPTIONS
 if( function_exists('acf_add_options_page') ) {
-
     acf_add_options_page(array(
         'page_title' 	=> 'Colores',
         'menu_title'	=> 'Colores',
--- src/_data/clients.js
+++ src/_data/clients.js
@@ -1,17 +1,17 @@
 const locales = require('./locales.js');
 const acf_optionGetByLang = require('../_helpers/acf_optionGetByLang.js');
-const type = "team";
+const type = "clients";
 
 async function getData(){
     console.log("------ Fetching team... ------ ");
 
-    const team = await acf_optionGetByLang(type);
+    const clients = await acf_optionGetByLang(type);
     const data_i18n = {};
 
     for (let i in locales.langs) {
         const lang = locales.langs[i].code;
         data_i18n[lang] = {};
-        data_i18n[lang].team = team[lang];
+        data_i18n[lang].clients = clients[lang];
     }
 
     return data_i18n;
